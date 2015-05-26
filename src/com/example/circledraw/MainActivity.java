00569f24477cf625683124f2e0d63d86b16d4858
himanshuarora <himanshu.arora@techbirds.in>
2015-05-26 11:27:58 +0530

    Values folder deleted

--- src/com/example/circledraw/MainActivity.java
+++ src/com/example/circledraw/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.circledraw;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
-	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
